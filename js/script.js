a265dd1c6ae4e8a85f4c77c24e9d542efbe38b8b
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-06 16:23:34 +0100

    add: array of elements

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const membersList = [
+
+    // Wayne
+    {
+        fullName: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    // Angela
+    {
+        fullName: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
+    },
+
+    // Walter
+    {
+        fullName: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
+    },
+
+    // Angela 2
+    {
+        fullName: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
+    },
+
+    // Scott
+    {
+        fullName: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg",
+    },
+
+    // Barbara
+    {
+        fullName: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
+    },
+
+];
+
+console.log(membersList);
